746318fa4230b3b09bb7df2bdfe04bc7654d7519
polyvanov <maybe.ya@yandex.ru>
2020-06-15 11:04:20 +0300

    Implemented - ready to deploy

--- src/main/java/com/voskhod/spnego/SpnegoApplication.java
+++ src/main/java/com/voskhod/spnego/SpnegoApplication.java
@@ -1,5 +1,6 @@
 package com.voskhod.spnego;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,17 +11,18 @@ import java.nio.file.Paths;
 @SpringBootApplication
 @EnableWebSecurity
 @ComponentScan("com.voskhod")
+@Log4j2
 public class SpnegoApplication {
 
-	static {
-		System.setProperty("java.security.krb5.conf",
-				Paths.get("/etc/krb5.conf")
-						.normalize().toAbsolutePath().toString());
-		System.setProperty("sun.security.krb5.debug", "true");
-	}
+    static {
+        System.setProperty("java.security.krb5.conf",
+                Paths.get("/etc/krb5.conf")
+                        .normalize().toAbsolutePath().toString());
+        System.setProperty("sun.security.krb5.debug", "true");
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpnegoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpnegoApplication.class, args);
+    }
 
 }

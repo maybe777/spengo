f884ca344b16d9c37607ae8bdc711722ad3d88ce
polyvanov <maybe.ya@yandex.ru>
2020-06-16 11:20:59 +0300

    Implemented - ready to deploy

--- src/main/java/com/voskhod/spnego/SpnegoApplication.java
+++ src/main/java/com/voskhod/spnego/SpnegoApplication.java
@@ -3,6 +3,7 @@ package com.voskhod.spnego;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -12,11 +13,11 @@ import java.nio.file.Paths;
 @EnableWebSecurity
 @ComponentScan("com.voskhod")
 @Log4j2
-public class SpnegoApplication {
+public class SpnegoApplication extends SpringBootServletInitializer {
 
     static {
         System.setProperty("java.security.krb5.conf",
-                Paths.get("/etc/krb5.conf")
+                Paths.get("/etc/ext/krb5.conf")
                         .normalize().toAbsolutePath().toString());
         System.setProperty("sun.security.krb5.debug", "true");
     }
--- src/main/java/com/voskhod/spnego/conf/KerberosSecurityConfig.java
+++ src/main/java/com/voskhod/spnego/conf/KerberosSecurityConfig.java
@@ -1,28 +1,28 @@
 package com.voskhod.spnego.conf;
 
-import com.voskhod.spnego.service.AppAuthenticationSuccessHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.kerberos.authentication.KerberosAuthenticationProvider;
-import org.springframework.security.kerberos.authentication.KerberosServiceAuthenticationProvider;
-import org.springframework.security.kerberos.authentication.sun.SunJaasKerberosClient;
-import org.springframework.security.kerberos.authentication.sun.SunJaasKerberosTicketValidator;
-import org.springframework.security.kerberos.web.authentication.SpnegoAuthenticationProcessingFilter;
-import org.springframework.security.kerberos.web.authentication.SpnegoEntryPoint;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.core.userdetails.User;
+        import com.voskhod.spnego.service.AppAuthenticationSuccessHandler;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.ComponentScan;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.core.io.FileSystemResource;
+        import org.springframework.security.authentication.AuthenticationManager;
+        import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+        import org.springframework.security.core.authority.AuthorityUtils;
+        import org.springframework.security.core.userdetails.UserDetailsService;
+        import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+        import org.springframework.security.kerberos.authentication.KerberosAuthenticationProvider;
+        import org.springframework.security.kerberos.authentication.KerberosServiceAuthenticationProvider;
+        import org.springframework.security.kerberos.authentication.sun.SunJaasKerberosClient;
+        import org.springframework.security.kerberos.authentication.sun.SunJaasKerberosTicketValidator;
+        import org.springframework.security.kerberos.web.authentication.SpnegoAuthenticationProcessingFilter;
+        import org.springframework.security.kerberos.web.authentication.SpnegoEntryPoint;
+        import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+        import org.springframework.security.core.userdetails.User;
 
 
 @Configuration
